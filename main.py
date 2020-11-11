330e047b50489ef0d61d637e0b002d153f85a400
akkuzu <74313161+akkuzu@users.noreply.github.com>
2020-11-11 18:58:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+## Costofthemeal = 25
+## Tax = Costofthemeal *8/100
+## Tip = Costofthemeal *10/100
+## Total = Costofthemeal + Tax + Tip
+## Print("Cost of the meal: ", Costofthemeal)
+##Print("Tax: ", Tax)
+##Print("Tip: ", Tip)
+##Print("Total cost of the meal: " , Total)
+
+
+Costofthemeal = int(input("Please enter the cost of your meal: "))
+Tax = Costofthemeal *8/100
+Tip = Costofthemeal *10/100
+Total = Costofthemeal+Tax+Tip
+
+print("Cost of the meal:",Costofthemeal)
+print ("Tax:", Tax)
+print ("Tip:", Tip)
+print("Total Cost of the Meal:", Total)
+
+
+## Costofthemeal = input("Please enter the cost of your meal: ")
+## print("Cost of the meal is: ", Costofthemeal)
+## print("Tax: ", Tax)
+## print("Tip: ", Tip)
+## print("Total cost of the meal: ", Total)
+
+
+
+
+
+
+
+
+
+
